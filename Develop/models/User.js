b77ff651be5843a6435f03a34fce8d55ec7a08d6
walterunderwood <walonsounderwood@gmail.com>
2022-05-06 16:14:49 -0700

    Creating user model

---
+++ Develop/models/User.js
@@ -0,0 +1,54 @@
+const { model, Datatypes, Model } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: Datatypes.INTEGRER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        password: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserPass) => {
+                newUserPass.password = await bcrypt.hash(newUserPass.password, 10);
+                return newUserPass;
+            }
+        },
+        sequelize: false,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
