408778e724811fbc6859526a06afbb6a6cd3d40b
walterunderwood <walonsounderwood@gmail.com>
2022-05-08 15:30:34 -0500

    Creating Relations between Models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -2,4 +2,31 @@ const Comment = require('./Comment');
 const Post = require('./Post');
 const User = require('./User');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsto(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'posted_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsto(Post, {
+    foreignKey: 'posted_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
 module.exports = { User, Post, Comment };
