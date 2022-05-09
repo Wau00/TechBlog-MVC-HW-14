080344120ffff581696a897942264c4483392a4c
walterunderwood <walonsounderwood@gmail.com>
2022-05-08 21:55:08 -0500

    Creating Post Route

--- Develop/controllers/api/post-routes.js
+++ Develop/controllers/api/post-routes.js
@@ -1,4 +1,62 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        })
+
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+
+    res.render('single-post');
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const postInfo = await Project.update({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.body.user_id,
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        if (!postInfo) {
+            res.status(404).json({ message: 'No project Found' });
+            return;
+        }
+        res.status(200).json(postInfo);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postInfo = await Project.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!postInfo) {
+            res.status(404).json({ message: 'No project Found' });
+            return;
+        }
+
+        res.status(200).json(postInfo);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
