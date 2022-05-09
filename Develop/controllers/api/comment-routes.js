cf799d4ef982d54e3fed601392a27d2e37daef9c
walterunderwood <walonsounderwood@gmail.com>
2022-05-08 23:04:25 -0500

    Creating Comment Route

--- Develop/controllers/api/comment-routes.js
+++ Develop/controllers/api/comment-routes.js
@@ -1,4 +1,42 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newComent = await Comment.create({
+            content: req.body.content,
+            posted_id: req.body.posted_id,
+            user_id: req.session.user_id
+        })
+
+        res.status(200).json(newComent);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+
+});
+
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const commentInfo = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!commentInfo) {
+            res.status(404).json({ message: 'No comment Found' });
+            return;
+        }
+
+        res.status(200).json(commentInfo);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 
