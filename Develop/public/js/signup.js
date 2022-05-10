9fd8b41da7f589c811b4b9fdafad6c573e86cb0a
walterunderwood <walonsounderwood@gmail.com>
2022-05-09 23:32:33 -0500

    Vhanges to input script

--- Develop/public/js/signup.js
+++ Develop/public/js/signup.js
@@ -1,15 +1,15 @@
 const signupFormHandler = async function (event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-input-signup').value.trim();
-    const password = document.querySelector('#password-input-signup').value.trim();
+    const username = document.querySelector('#InputUser').value.trim();
+    const email = document.querySelector('#InputEmail').value.trim();
+    const password = document.querySelector('#InputPassword').value.trim();
     console.log(username);
+    console.log(email);
     console.log(password);
     const response = await fetch('/api/user', {
         method: 'POST',
-        body: JSON.stringify({
-            username, password
-        }),
+        body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
     });
 
