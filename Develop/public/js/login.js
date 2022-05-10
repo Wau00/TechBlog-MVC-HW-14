0c4cfb8d85c5260ea4f7f5465cb547b1557e4e90
walterunderwood <walonsounderwood@gmail.com>
2022-05-09 22:32:51 -0500

    Sourcing and creating front-end javascripts

---
+++ Develop/public/js/login.js
@@ -0,0 +1,23 @@
+const loginFormHandler = async function (event) {
+    event.preventDefault();
+
+    const usernameEl = document.querySelector('#InputUser').value.trim();
+    const passwordEl = document.querySelector('#InputPassword').value.trim();
+    if (usernameEl && passwordEl) {
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({ usernameEl, passwordEl }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to login');
+        }
+    }
+};
+
+document
+    .querySelector('#login-form')
+    .addEventListener('submit', loginFormHandler);
